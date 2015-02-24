2a724755a61a2131a28d47a3f472d0d71eccf920
Karel Hübl <karel.huebl@gmail.com>
2015-02-24 20:00:39 +0100

    Added traversing functionality to MetaFactory.

--- disl/src/org/disl/meta/MetaFactory.groovy
+++ disl/src/org/disl/meta/MetaFactory.groovy
@@ -1,5 +1,9 @@
 package org.disl.meta
 
+import groovy.io.FileType
+
+import java.lang.reflect.Modifier
+
 class MetaFactory {
 	static Object create(Class type) {
 		def instance=type.newInstance();
@@ -7,5 +11,30 @@ class MetaFactory {
 			instance.init();
 		}
 		instance
+	}
+
+	/**
+	 * Traverse all class files in traversePath and creates instances for all found classes in given rootPackage (including subpackages) which are assignable to assignableType.
+	 * 
+	 * Example: 
+	 * //Generate all tables defined in disl model for your data warehouse.
+	 * MetaFactory.createAll("bin","com.yourDw",Table).each({it.generate})
+	 * */
+	static Collection createAll(String traversePath,String rootPackage,Class assignableType) {
+		def typesToCreate=findTypes(traversePath,{it.getName().startsWith(rootPackage) && assignableType.isAssignableFrom(it) && !Modifier.isAbstract(it.getModifiers())})
+		typesToCreate.collect {create(it)}
+	}
+
+	private static Collection<Class> findTypes(String traversePath,Closure classFilter) {
+		File traverseDir = new File(traversePath)
+		def filterClassFiles = ~/.*\.class$/
+		def types=[]
+		traverseDir.traverse (type: FileType.FILES, nameFilter: filterClassFiles){
+			String classFile=it.absolutePath.substring(traverseDir.absolutePath.length()+1)
+			String className=classFile.substring(0,classFile.length()-6).replace('\\', '.')
+			def type=Class.forName(className)
+			types.add(type)
+		}
+		types.findAll classFilter
 	}
 }
