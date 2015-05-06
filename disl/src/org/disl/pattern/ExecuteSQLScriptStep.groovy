d6f3c9ad867acb0016395b8a68b43f361291db9f
Karel Hübl <karel.huebl@gmail.com>
2015-05-06 16:01:03 +0200

    Refactoring.

--- disl/src/org/disl/pattern/ExecuteSQLScriptStep.groovy
+++ disl/src/org/disl/pattern/ExecuteSQLScriptStep.groovy
@@ -8,6 +8,9 @@ import org.junit.Assert
 import org.junit.Test
 
 public class ExecuteSQLScriptStep extends Step {
+
+	public static final String BACKSLASH_NEW_LINE = "\\\\\n"
+	
 	boolean ignoreErrors=false;
 	String commandSeparator=";";
 	int updatedRowCount;
@@ -23,7 +26,7 @@ public class ExecuteSQLScriptStep extends Step {
 	@Override
 	public void execute() {
 		try {
-		code.split(commandSeparator).each {
+		getCommands().each {
 			executeSqlStatement(it)
 		}
 		getSql().commit()
@@ -31,6 +34,10 @@ public class ExecuteSQLScriptStep extends Step {
 			getSql().rollback()
 			throw e
 		}
+	}
+	
+	protected Collection<String> getCommands() {
+		return code.split(commandSeparator)
 	}
 
 	protected executeSqlStatement(String sqlCommand) {
@@ -51,5 +58,17 @@ public class ExecuteSQLScriptStep extends Step {
 		def element=new Base()
 		pattern="DROP ${element.name}"
 		Assert.assertEquals("DROP Base", code)
+	}
+	
+	@Test
+	void testGetCommands() {
+		def p=new ExecuteSQLScriptStep(commandSeparator: BACKSLASH_NEW_LINE,pattern: "A;B")
+		Assert.assertEquals(1, p.getCommands().size())
+		Assert.assertEquals("A;B", p.getCommands()[0])
+		p=new ExecuteSQLScriptStep(pattern: "A;B");
+		Assert.assertEquals(2, p.getCommands().size())
+		Assert.assertEquals("A", p.getCommands()[0])
+		Assert.assertEquals("B", p.getCommands()[1])
+		
 	}
 }
