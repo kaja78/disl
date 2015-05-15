248b70aa3189eed5f225ae7259d0916c4fab0dbe
Karel Hübl <karel.huebl@gmail.com>
2015-05-15 14:31:00 +0200

    fix implements Executable

--- disl/src/main/groovy/org/disl/meta/TableMapping.groovy
+++ disl/src/main/groovy/org/disl/meta/TableMapping.groovy
@@ -1,8 +1,9 @@
 package org.disl.meta
 
+import org.disl.pattern.Executable;
 import org.disl.pattern.Pattern;
 
-abstract class TableMapping extends Mapping{
+abstract class TableMapping extends Mapping implements Executable {
 	public abstract Table getTarget()
 	public abstract Pattern getPattern()
 	
