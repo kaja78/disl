a0aa1670c5b32f2396429dbc74e00090375d4722
antoninkrotky <antoninkrotky@gmail.com>
2015-05-21 17:54:20 +0200

    Update Column.groovy

--- disl/src/main/groovy/org/disl/meta/Column.groovy
+++ disl/src/main/groovy/org/disl/meta/Column.groovy
@@ -7,21 +7,20 @@ class Column {
 	String dataType
 
 	Column(){}
-
+	
 	Column(String name,Table parent) {
 		this.name=name
 		this.parent=parent
 	}
-
+	
 	String getColumnDefinition() {
 		"${getName()} ${getDataType()}"
 	}
-
+	
 	String toString(){
 		if (parent==null || parent.getSourceAlias()==null) {
 			return name
 		}
 		"${parent.getSourceAlias()}.${getName()}"
 	}
-
 }
