0c4dc51673cf0224aea8cca3f75a785061d7a8e8
Karel Hübl <karel.huebl@gmail.com>
2015-06-16 11:28:29 +0200

    Added licences.

--- disl/src/main/groovy/org/disl/meta/IndexMeta.groovy
+++ disl/src/main/groovy/org/disl/meta/IndexMeta.groovy
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2015 Karel Hübl <karel.huebl@gmail.com>.
+ *
+ * This file is part of disl.
+ *
+ * Disl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Disl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Disl.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.disl.meta;
 
 import java.util.List;
