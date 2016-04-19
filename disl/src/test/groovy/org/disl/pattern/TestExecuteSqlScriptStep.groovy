79eb2c68810d59bc11814fe18dd3b79709351da1
Karel Hübl <karel.huebl@gmail.com>
2016-04-19 17:02:29 +0200

    Added junit test for BACKSLASH_NEWLINE separator.

--- disl/src/test/groovy/org/disl/pattern/TestExecuteSqlScriptStep.groovy
+++ disl/src/test/groovy/org/disl/pattern/TestExecuteSqlScriptStep.groovy
@@ -35,6 +35,33 @@ class TestExecuteSqlScriptStep extends DislTestCase {
 		assertEquals(2, p.getCommands().size())
 		assertEquals("A", p.getCommands()[0])
 		assertEquals("B", p.getCommands()[1])
+		p=new TestingExecuteSQLScriptStep(commandSeparator: ExecuteSQLScriptStep.BACKSLASH_NEW_LINE ,code: """\
+/*1st block*/
+begin
+	null;
+end;
+\\
+/*2nd block*/
+begin
+	null;
+end;
+""")
+		def commands=p.getCommands()
+		assertEquals(2, p.getCommands().size())
+		assertEquals("""\
+/*1st block*/
+begin
+	null;
+end;
+""", p.getCommands()[0])
+		assertEquals("""\
+/*2nd block*/
+begin
+	null;
+end;
+""", p.getCommands()[1])
+			
+		
 	}
 	
 	static class TestingExecuteSQLScriptStep extends ExecuteSQLScriptStep {
