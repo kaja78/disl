db6e8bb5e0542113f95b323bf78a7d6cb374dd10
Karel Hubl <karel.hubl@collibra.com>
2021-05-20 16:05:26 +0200

    Implement logging in FileOutputStep.

--- src/main/groovy/org/disl/pattern/FileOutputStep.groovy
+++ src/main/groovy/org/disl/pattern/FileOutputStep.groovy
@@ -19,13 +19,14 @@
  */
 package org.disl.pattern
 
-import java.io.File
-
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 
-import java.nio.charset.Charset;;
+import java.nio.charset.Charset
+;
 
 @CompileStatic
+@Slf4j
 abstract class FileOutputStep extends Step {
 
 	String charset
@@ -41,9 +42,12 @@ abstract class FileOutputStep extends Step {
 	
 	@Override
 	public int executeInternal() {
-		file.getParentFile().mkdirs()
+		if (file.getParentFile().mkdirs()) {
+			log.info("Created directory ${file.getParentFile().getAbsolutePath()}")
+		}
 		file.createNewFile()
 		file.write(getCode(),getCharset())
+		log.info("Created file ${file.getAbsolutePath()}")
 		return 1
 	}
 
